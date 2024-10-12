8f5fbf561dd2034616922c1ff2a085c732908c7a
DMI2002ATIITM <ns24z353@smail.iitm.ac.in>
2024-10-12 12:15:59 +0530

    Made fixes to Signed_8bitMul bsv code and testbench, now it is working (Need to take care of handshaking at the end)

--- Unpipelined/MAC_int32/Signed_8bitMul.bsv
+++ Unpipelined/MAC_int32/Signed_8bitMul.bsv
@@ -8,43 +8,50 @@ endinterface: Ifc_Signed_8bitMul
 
 (* synthesize *)
 module mkSigned_8bitMul(Ifc_Signed_8bitMul);
-Reg#(Bit#(8)) rg_A <- mkReg(0);
-Reg#(Bit#(8)) rg_B <- mkReg(0);
+Reg#(Bit#(16)) rg_A <- mkReg(0);
+Reg#(Bit#(16)) rg_B <- mkReg(0);
 Reg#(Bit#(16)) rg_AB <- mkReg(0);
 Reg#(Bool) got_A <- mkReg(False);
 Reg#(Bool) got_B <- mkReg(False);
 Reg#(Bool) completed <- mkReg(False);
 Reg#(Bit#(16)) partial_store <- mkReg(0);
+Reg#(Bit#(5)) count <- mkReg(16);
 
-rule rl_multiply(got_A && got_B && rg_B != 8'd0);
-completed <= False;
+rule rl_multiply(got_A && got_B && count != 5'd0);
 if(rg_B[0] == 1)
 begin
-	partial_store <= partial_store + signExtend(rg_A); // TODO Must implement "+" as adder
+	partial_store <= partial_store + rg_A; // TODO Must implement "+" as adder
 end
 rg_A <= rg_A << 1;
 rg_B <= rg_B >> 1;
+count <= count - 1; 
+if(count == 5'd1)
+begin
+	completed <= True;
+end
 endrule
 
 method Action get_A(Bit#(8) a) if(! got_A);
-rg_A <= a;
+completed <= False;
+rg_A <= signExtend(a);
 got_A <= True;
 endmethod 
 
 method Action get_B(Bit#(8) b) if(! got_B);
-rg_B <= b;
+rg_B <= signExtend(b);
 got_B <= True;
 endmethod 
 
 method ActionValue #(Bit#(16)) output_Mul();
-if(rg_B == 8'd0)
+if(completed == True)
 begin
 	got_A <= False;
 	got_B <= False;
-	completed <= True;
 	rg_AB <= partial_store;
+	partial_store <= 0;
+	count <= 16;
 end
-return rg_AB;
+return signExtend(rg_AB);
 endmethod 
 
 
--- Unpipelined/MAC_int32/Signed_8bitMul_verif/test_Signed_8bitMul.py
+++ Unpipelined/MAC_int32/Signed_8bitMul_verif/test_Signed_8bitMul.py
@@ -1,14 +1,12 @@
-# This file is public domain, it can be freely copied without restrictions.
-# SPDX-License-Identifier: CC0-1.0
-
 import os
 import random
 from pathlib import Path
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge
-#from model_counter import *
+from cocotb.triggers import RisingEdge, ClockCycles
+import logging as _log
+
 
 # Ports:
 # Name                         I/O  size props
@@ -24,43 +22,61 @@ from cocotb.triggers import RisingEdge
 # EN_get_B                       I     1
 # EN_output_Mul                  I     1
 
-@cocotb.test()
-async def test_counter(dut):
-    """Test to check counter"""
-#
-#    dut.EN_increment.value = 0
-#    dut.EN_decrement.value = 0
-#
-    clock = Clock(dut.CLK, 10, units="us")  # Create a 10us period clock on port clk
-    # Start the clock. Start it low to avoid issues on the first RisingEdge
-    cocotb.start_soon(clock.start(start_high=False))
+async def reset(dut):
+    dut.RST_N.value = 1
+    await RisingEdge(dut.CLK)
     dut.RST_N.value = 0
     await RisingEdge(dut.CLK)
     dut.RST_N.value = 1
     await RisingEdge(dut.CLK)
-    dut.get_A_a.value = -2
-    dut.get_B_b.value = 5
-    await RisingEdge(dut.CLK)
+
+
+async def give_input(dut,A,B):
+    dut.get_A_a.value = A
+    dut.get_B_b.value = B
     await RisingEdge(dut.CLK)
     dut.EN_get_A.value = 1
     dut.EN_get_B.value = 1
     await RisingEdge(dut.CLK)
     dut.EN_get_A.value = 0
     dut.EN_get_B.value = 0
-    await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
+    
+async def get_output(dut):
+    await RisingEdge(dut.completed)
     dut.EN_output_Mul.value = 1
     await RisingEdge(dut.CLK)
     await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
-    await RisingEdge(dut.CLK)
+    rtl_answer =  dut.output_Mul.value
+    dut.EN_output_Mul.value = 0
+    #print(bin(int(str(rtl_answer),2)^ 0xFFFF + 1))
+    #print(int(str(rtl_answer),2) ^ 0xFFFF + 1)
+    str_ans = str(rtl_answer)
+    if(str_ans[0] == "1"):
+        rtl_answer = int(bin((int(str_ans,2) ^ 0xFFFF) + 1)[2:],2) * -1
+    else:
+        rtl_answer = int(str(rtl_answer),2)
+    return rtl_answer
+
+	
+
+@cocotb.test()
+async def test_mul(dut):
+    clock = Clock(dut.CLK, 10, units="us")  
+    cocotb.start_soon(clock.start(start_high=False))
+    await reset(dut)
+    
+    A = list(range(-128,3))
+    B = list(range(0,127))
+    #B = [-1] * len(A)
+    
+    for i in A:
+        for j in B:
+            await give_input(dut,i,j)
+            await ClockCycles(dut.CLK, 10)
+            rtl_result = await get_output(dut)
+            await ClockCycles(dut.CLK, 1)
+            print()
+            print(str(i) + " " + str(j) + " " + str(i*j) + " == " +   str(rtl_result))
+            assert i*j == rtl_result
     
 

a3db62a5804c82404356e7f371107ec7c7683f41
DMI2002ATIITM <ns24z353@smail.iitm.ac.in>
2024-10-19 12:49:22 +0530

    Added seperate reference model for float multiplication and float addition for easier debugging of failure cases

---
+++ Unpipelined/MAC_fp32/ADD_RM.py
@@ -0,0 +1,90 @@
+           
+def round_fp32(A):
+    # input and output are str
+    round_bit = A[23]
+    if(round_bit == "0"):
+        return A[:23]
+    elif(round_bit == "1"):
+        temp = A[24:]
+        if(int(temp,2) == 0):
+            return A[:22]+"0"
+        else:
+            return bin(int(A[:23],2)+1)[2:].rjust(23,"0")            
+
+def decode_fp32(A):
+    A_sign = A[0]
+    A_exp =  int(A[1:9],2) - 127
+    A_frac =  A[9:]
+
+    L = list(range(1,32))
+    for i in range(len(L)):
+        L[i] = 1/2**L[i]
+
+    ans = 0
+    for i in range(len(A_frac)):
+        if(A_frac[i] == "1"):
+            ans += L[i]
+
+    ans += 1
+    ans = ans * 2**A_exp
+
+    if(A_sign == "1"):
+        ans = ans * -1
+
+    return ans
+
+def fp32_add(A,B):
+    A_sign = A[0]
+    A_exp =  int(A[1:9],2)
+    A_frac =  "1"+A[9:].ljust(23,"0")
+
+    B_sign = B[0]
+    B_exp =  int(B[1:9],2)
+    B_frac =  "1"+B[9:]
+
+    exp_diff = A_exp - B_exp
+
+    if(exp_diff < 0):
+        print("1")
+
+        A_sign, B_sign = B_sign, A_sign
+        A_exp, B_exp = B_exp, A_exp
+        A_frac, B_frac = B_frac, A_frac
+
+
+    for i in range(abs(exp_diff)):
+        # print("2")
+        B_frac = "0" + B_frac
+
+    Blen_bef_add = len(B_frac)
+    C_exp = A_exp
+
+    print(A_sign,B_sign)
+    if(A_sign == B_sign):
+        print("3")
+        # addition ...
+        A_frac = A_frac.ljust(Blen_bef_add,"0")
+        # print(Blen_bef_add)
+        print(A_frac)
+        # print(B_frac)
+        sum_val = bin(int(A_frac,2) + int(B_frac,2))[2:]
+        print(sum_val)
+        # carry detection
+        if(len(sum_val) > Blen_bef_add):
+            print("4")
+            # Adjust exponent
+            exp_add_diff = len(sum_val) - Blen_bef_add
+            A_exp += exp_add_diff
+        rounded_sum = round_fp32(sum_val[1:])
+        return A_sign + bin(A_exp)[2:].rjust(8,"0") + rounded_sum
+    else:
+        print("5")
+        # subtraction ...
+        return None
+A = "0101011010011010"
+B = "01000001110010101100001010010000"
+Add = fp32_add(A,B)
+print(Add)
+
+# 01010110100110100000000000000000
+# 01010110100110100000000000000000
---
+++ Unpipelined/MAC_fp32/MUL_RM.py
@@ -0,0 +1,109 @@
+def round_bfloat16(A):
+    # input and output are str
+    A += "0" # Adding for safety
+    print("A",A)
+    round_bit = A[7]
+    if(round_bit == "0"):
+        return A[:7]
+    # return A[:7]
+    elif(round_bit == "1"):
+        temp = A[8:]
+        if(int(temp,2) == 0):
+            return A[:6]+"0"
+        else:
+            print("here")
+            return bin(int(A[:7],2)+1)[2:].rjust(7,"0")
+        # return A[:6]+"0"
+
+def bfloat16_mul(A,B):
+    A_sign = int(A[0],2)
+    A_exp =  int(A[1:9],2)
+    A_frac =  int("1"+A[9:],2)
+
+    B_sign = int(B[0],2)
+    B_exp =  int(B[1:9],2)
+    B_frac =  int("1"+B[9:],2)
+
+    # bias = int("1111111",2)
+    bias = int("10000001",2)
+    
+    AB_sign = bin(A_sign ^ B_sign)[2:]
+
+    # AB_exp = bin(A_exp + B_exp - bias)[2:][-8:]
+    # AB_exp = bin(A_exp + B_exp + bias)[2:][-8:]
+
+    AB_exp = bin((A_exp + B_exp + bias) & 0xFF)[2:][-8:]
+    print(AB_exp)
+
+    # Multiplication of mantissa
+    temp_A = bin(A_frac)[2:]
+    for i in range(len(temp_A)):
+        if(temp_A[-1] == "1"):
+            break
+        temp_A = temp_A[:-1]
+
+    temp_B = bin(B_frac)[2:]
+    for i in range(len(temp_B)):
+        if(temp_B[-1] == "1"):
+            break
+        temp_B = temp_B[:-1]
+
+    nob_A = len(temp_A)
+    nob_B = len(temp_B)
+    print(nob_A,nob_B)
+
+    temp_AB = int(temp_A,2) * int(temp_B,2)
+    nob_AB = len(bin(temp_AB)[2:])
+
+    exp_adj = nob_AB - (nob_A + nob_B - 1) 
+    print(exp_adj)
+    AB_exp = bin(int(AB_exp,2) + exp_adj)[2:]
+
+    print(f"before rounding {bin(A_frac * B_frac)[2:]}")
+    AB_frac = round_bfloat16(bin(A_frac * B_frac)[3:])
+    # AB_frac = bin(A_frac * B_frac)[3:]
+
+    return AB_sign + AB_exp.rjust(8,"0") + AB_frac
+
+
+def decode_bfloat_16(A):
+    A_sign = A[0]
+    A_exp =  int(A[1:9],2) - 127
+    A_frac =  A[9:]
+
+    L = list(range(1,24))
+    for i in range(len(L)):
+        L[i] = 1/2**L[i]
+
+    ans = 0
+    for i in range(len(A_frac)):
+        if(A_frac[i] == "1"):
+            ans += L[i]
+
+    ans += 1
+    ans = ans * 2**A_exp
+
+    if(A_sign == "1"):
+        ans = ans * -1
+
+    return ans
+
+A = "0100000100100000"
+B = "0011111000100011"
+AB = bfloat16_mul(A,B)
+print(AB)
+
+
+# Test 1
+# 0 10101101 0011001|1|10111           <- manual calculation
+# 0 10101101 0011010           <- manual calculation (rounded)
+# 0 10101101 00110100000000000000000 <- Website result
+# 0 10101101 0011010            <- RM output
+
+
+# Test 30
+# 0 01111111 1001100   <- RM output
+# 0 01111111 100101111  <- manual calculation
+# 0 01111111 10010111111111111111111 <- website result
+
+
